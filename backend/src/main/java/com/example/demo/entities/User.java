a8714357dea013f9816a4654d1c419d5783ea13f
AdityaFand <154223055+AdityaFand@users.noreply.github.com>
2024-02-20 11:05:22 +0530

    Update User.java

--- backend/src/main/java/com/example/demo/entities/User.java
+++ backend/src/main/java/com/example/demo/entities/User.java
@@ -1,8 +1,6 @@
 package com.example.demo.entities;
 
-import java.util.Set;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,10 +8,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
