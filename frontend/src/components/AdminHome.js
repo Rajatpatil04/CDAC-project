575c852ab81f5a965bafda06a77fb13910657498
Rajatpatil04 <rajatpatilftbl@gmail.com>
2024-02-24 09:16:16 +0530

    dot.net

--- frontend/src/components/AdminHome.js
+++ frontend/src/components/AdminHome.js
@@ -4,32 +4,11 @@ import { Link, NavLink, Outlet } from "react-router-dom"
 import favicon from '../images/favicon-removebg-preview.png';
 
 export default function AdminHome(){
-  
+  var name = JSON.parse(localStorage.getItem("loggedUser")).username; 
   return(
-      <div>
-        {/* <Navbar bg="light" expand="lg" className='navcol'>
-        <div className="container-fluid">
-          <Link to="/" className="navbar-brand">
-            <img src={favicon} alt="Book My Car Logo" width="90" height="40" className="d-inline-block align-text-top"/>
-          </Link>
-          <h1 className="text-dark" style={{ fontFamily: "-moz-initial" }}>BOOK MY CAR</h1>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <div className="input-group search">
-              <input type="search" className="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
-              <button type="button" className="btn btn-outline-primary" data-mdb-ripple-init>search</button>
-            </div>
-            
-            <Nav className="ml-auto">
-              {/* <NavLink to ="/" className="nav-link text-danger"><h5 style={{ fontFamily: "serif" }}><b>HOME</b></h5></NavLink> */}
-              {/* <NavLink to ="/viewcars" className="nav-link text-danger"><h5 style={{ fontFamily: "serif" }}><b>CARS</b></h5></NavLink>
-              <NavLink to ="/logout" className="nav-link text-danger"><h5 style={{ fontFamily: "serif" }}><b>LOGOUT</b></h5></NavLink>
-              <NavLink to ="/requests" className="nav-link text-danger"><h5 style={{ fontFamily: "serif" }}><b>REQUESTS</b></h5></NavLink>
-            </Nav> */}
-          {/* </Navbar.Collapse> */}
-        {/* </div> */}
-      {/* </Navbar> */} 
-        <h1>Admin Home</h1>
+      <div className='abody container-fluid'>
+     
+     <h1 style={{fontFamily:"revert"}}> Welcome {name} </h1>
         <Outlet/>
       </div>
   )
--- frontend/src/components/HostHome.js
+++ frontend/src/components/HostHome.js
@@ -4,11 +4,11 @@ import { Link, NavLink, Outlet } from "react-router-dom"
 
 
 export default function HostHome(){
+  var name = JSON.parse(localStorage.getItem("loggedUser")).username; 
   const mystate = useSelector(state=>state.logged)
   return(
-      <div className='hbody'>
-     
-      
+      <div className='hbody'>   
+     <h1 style={{fontFamily:"revert"}}> Welcome {name} </h1>
         <Outlet/>
       </div>
   )
